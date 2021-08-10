639e352468e66ed21824f0a1c401d0db39735122
aksvelichko <aksvelichko@gmail.com>
2021-08-10 17:28:30 +0300

    first

---
+++ app/config/db.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // const { MongoClient } = require('mongodb');
+    // const uri = "mongodb+srv://aksVelichko:aks100720@cluster0.d8wxk.mongodb.net/Node.js?retryWrites=true&w=majority";
+    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    // client.connect(err => {
+    //   const collection = client.db("test").collection("devices");
+    //   // perform actions on the collection object
+    //   client.close();
+    // });
+
+    uri: "mongodb+srv://aksVelichko:aks100720@cluster0.d8wxk.mongodb.net/Node?retryWrites=true&w=majority"
+  };
